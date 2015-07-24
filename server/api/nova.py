16a5947224579dd455d8dfa6b732655e3e7c3ea5
Ali Shariat <shariat@gmail.com>
2015-07-24 17:10:56 -0400

    return nova list result

--- server/api/nova.py
+++ server/api/nova.py
@@ -2,7 +2,9 @@ from __future__ import absolute_import
 from flask_restful import Resource, current_app
 from . import rest_api
 import requests
-import json
+
+NOVA_PORT = ":8774/v2"
+AUTH_PORT = ":5000/v2.0"
 
 
 class NovaResources(Resource):
@@ -16,9 +18,17 @@ class NovaResources(Resource):
                 }
             }
         }
-        print payload
-        print current_app.config["OS_AUTH_URL"]
-        res = requests.post(current_app.config["OS_AUTH_URL"] + "/tokens", json=payload)
+        AUTH_URL = "%s%s/tokens" % (current_app.config["OS_SERVER"], AUTH_PORT)
+        NOVA_URL = "%s%s" % (current_app.config["OS_SERVER"], NOVA_PORT)
+
+        res = requests.post(AUTH_URL, json=payload)
+        auth_res = res.json()
+        token = auth_res["access"]["token"]["id"]
+        tenant_id = auth_res["access"]["token"]["tenant"]["id"]
+        nova_url = "%s/%s/%s" % (NOVA_URL, tenant_id, "servers")
+        res = requests.get(nova_url,
+                           headers={'Content-Type': 'application/json',
+                                    'X-Auth-Token': token})
         return res.json()
 
 rest_api.add_resource(NovaResources, '/nova')
