d2aaa1b31d91436a658eb4b744eaaa75a7c02f60
Ali Shariat <shariat@gmail.com>
2015-07-25 13:37:07 -0400

    use nova python lib

--- server/api/nova.py
+++ server/api/nova.py
@@ -1,42 +1,25 @@
 from __future__ import absolute_import
 from flask_restful import Resource, current_app, fields, marshal_with
 from . import rest_api
-import requests
-
-NOVA_PORT = ":8774/v2"
-AUTH_PORT = ":5000/v2.0"
+from novaclient import client
 
 NOVA_FIELDS = {
-    "servers": fields.List(fields.Nested({
-            "id": fields.String(attribute="id"),
-            "name": fields.String(attribute="name")
-        }))
+    "id": fields.String(attribute="id"),
+    "name": fields.String(attribute="name")
 }
 
 
 class NovaResources(Resource):
     @marshal_with(NOVA_FIELDS)
     def get(self):
-        payload = {
-            "auth": {
-                "tenantName": current_app.config["OS_TENANT_NAME"],
-                "passwordCredentials": {
-                    "username": current_app.config["OS_USERNAME"],
-                    "password": current_app.config["OS_PASSWORD"]
-                }
-            }
-        }
-        AUTH_URL = "%s%s/tokens" % (current_app.config["OS_SERVER"], AUTH_PORT)
-        NOVA_URL = "%s%s" % (current_app.config["OS_SERVER"], NOVA_PORT)
-
-        res = requests.post(AUTH_URL, json=payload)
-        auth_res = res.json()
-        token = auth_res["access"]["token"]["id"]
-        tenant_id = auth_res["access"]["token"]["tenant"]["id"]
-        nova_url = "%s/%s/%s" % (NOVA_URL, tenant_id, "servers")
-        res = requests.get(nova_url,
-                           headers={'Content-Type': 'application/json',
-                                    'X-Auth-Token': token})
-        return res.json()
+        OS_USERNAME = current_app.config["OS_USERNAME"]
+        OS_PASSWORD = current_app.config["OS_PASSWORD"]
+        OS_TENANT_NAME = current_app.config["OS_TENANT_NAME"]
+        AUTH_URL = current_app.config["OS_AUTH_URL"]
+        nova = client.Client(2, OS_USERNAME,
+                             OS_PASSWORD,
+                             OS_TENANT_NAME,
+                             auth_url=AUTH_URL)
+        return nova.servers.list()
 
 rest_api.add_resource(NovaResources, '/nova')
